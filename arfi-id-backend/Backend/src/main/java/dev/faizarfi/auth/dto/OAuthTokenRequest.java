43aa221eb4f295c62499016f2739bad35927d96a
Faiz Arfi <faizarfi12@gamil.com>
2026-06-03 00:41:40 +0530

    add JSON property annotations to OAuthTokenRequest fields

--- arfi-id-backend/Backend/src/main/java/dev/faizarfi/auth/dto/OAuthTokenRequest.java
+++ arfi-id-backend/Backend/src/main/java/dev/faizarfi/auth/dto/OAuthTokenRequest.java
@@ -1,5 +1,6 @@
 package dev.faizarfi.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,17 +15,21 @@ public class OAuthTokenRequest {
 
     // must be "authorization_code" for authorization code flow
     @NotBlank(message = "Grant type is required")
+    @JsonProperty("grant_type")
     private String grantType;
 
     @NotBlank(message = "Authorization code is required")
     private String code;
 
     @NotBlank(message = "Redirect URI is required")
+    @JsonProperty("redirect_uri")
     private String redirectUri;
 
     @NotBlank(message = "Client ID is required")
+    @JsonProperty("client_id")
     private String clientId;
 
     @NotBlank(message = "Client secret is required")
+    @JsonProperty("client_secret")
     private String clientSecret;
 }
